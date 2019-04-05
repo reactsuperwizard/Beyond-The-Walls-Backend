612e6ebb20b07fda53edc3053f35f1b6b5246adb
bahu1013 <xu.yong@yandex.com>
2019-04-05 12:05:37 +0800

    remove aws config

--- Config/awsS3Config.js
+++ Config/awsS3Config.js
@@ -1,10 +1,10 @@
 'use strict';
 var s3BucketCredentials = {
-    "bucket": "beyondthewalls",
-    "accessKeyId": "AKIAJYD7Q5CGKUIJSEIQ",
-    "secretAccessKey": "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",
-    "s3URL": "https://s3-us-west-2.amazonaws.com",
-    "folder": "beyondthewalls"
+    "bucket": "",
+    "accessKeyId": "",
+    "secretAccessKey": "",
+    "s3URL": "",
+    "folder": ""
 };
 
 module.exports = {
